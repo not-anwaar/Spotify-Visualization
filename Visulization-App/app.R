e8d53a7e33bd04ed543c549d50d4440de2ccf5e1
Anwaar Hadi <anwaar.hadi@gmail.com>
2024-12-02 01:07:22 -0600

    3d plotting functionality

--- Visulization-App/app.R
+++ Visulization-App/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ggplot2)
+library(plotly)
 
 song_data <- read.csv("Spotify-2000.csv")
 numeric_cols <- c("Index", 
@@ -20,27 +21,57 @@ ui <- fluidPage(
   titlePanel("Spotify Data Visualization"),
   sidebarLayout(
     sidebarPanel(
+      selectInput("dim", "Dimension:", 
+                  choices = c("2D", "3D"),
+                  selected = "2D"),
       selectInput("x_var", "X-axis:",
                   choices = numeric_cols,
                   selected = "Year"),
       selectInput("y_var", "Y-axis",
                   choices = numeric_cols,
-                  selected = "Danceability")
+                  selected = "Danceability"),
+      selectInput("z_var", "Z-axis",
+                  choices = numeric_cols,
+                  selected = "Energy")
     ),
     mainPanel(
-      plotOutput("scatterPlot")
+      plotlyOutput("scatterPlot"),
     )
   )
 )
 
 server <- function(input, output) {
-  output$scatterPlot <- renderPlot({
-    ggplot(data = song_data, aes_string(x = input$x_var, y = input$y_var)) +
-      geom_point(color = "blue", size = 3) +
-      labs(title = paste("Scatter Plot of", input$y_var, "vs", input$x_var),
-           x = input$x_var,
-           y = input$y_var) +
-      theme_grey()
+  dimension <- reactive({
+    dimension <- input$dim
+  })
+  observe({
+    if (dimension() == "3D") {
+      output$scatterPlot <- renderPlotly({
+        plot_ly(data=song_data, 
+                x = ~get(input$x_var), 
+                y = ~get(input$y_var), 
+                z = ~get(input$z_var), 
+                type="scatter3d", 
+                mode="markers") %>%
+          layout(
+            title = paste(input$z_var, "as a function of", input$x_var, "and", input$y_var),
+            scene = list(
+              xaxis = list(title = paste("", input$x_var)),
+              yaxis = list(title = paste("", input$y_var)),
+              zaxis = list(title = paste("", input$z_var))
+            )
+          )
+      })
+    } else {
+      output$scatterPlot <- renderPlotly({
+        ggplot(data = song_data, aes_string(x = input$x_var, y = input$y_var)) +
+          geom_point(color = "blue", size = 3) +
+          labs(title = paste("Scatter Plot of", input$y_var, "vs", input$x_var),
+               x = input$x_var,
+               y = input$y_var) +
+          theme_grey()
+      })
+    }
   })
 }
 
